9e6ee3b3c6904482eb327d8c149cd41bf2f15e5e
Sami-Ali40 <118719782+Sami-Ali40@users.noreply.github.com>
2022-11-28 12:54:39 +0500

    A40.py
    Cd
    clear
    pip install stdiomask
    rm -rf A40
    git clone https://github.com/Sami-Ali40/A40.git
    cd A40
    python A40.py

---
+++ A40.py
@@ -0,0 +1,63 @@
+# SAMI
+
+![logo](https://i.postimg.cc/pXDWHY6D/Screenshot-20200930-062011-Chrome.jpg)
+
+* **Visitor Github @Sami-Ali40**
+
+![Sami-Ali40](https://komarev.com/ghpvc/?username=SAMI&color=blue)
+
+<a href="https://github.com/Sami-Ali40/followers">
+
+<img title="Followers" src="https://img.shields.io/github/followers/Sami-Ali40?label=Followers&color=red&style=flat-square"></a>
+
+### 👤 My Accounts 👤
+
+[![](https://img.shields.io/badge/Facebook-blue?logo=Facebook&logoColor=blue&labelColor=white)]https://www.facebook.com/profile.php?id=100076381339868
+
+[![](https://img.shields.io/badge/Messenger-red?logo=Messenger&logoColor=red&labelColor=black)](https://m.me/Sami.bukhari) <br>
+
+[![](https://img.shields.io/badge/Whatsapp-CHAT-red?logo=Whatsapp&logoColor=Brightgreen&labelColor=white)](https://wa.me/923163372011?text=Hello+Meer+Shahzeb) <br><br>
+
+## 📸 Screenshot 📸
+
+![Screenshot_2022-08-29-17-44-00-945_com termux](https://github.com/Sami-Ali40/A40.git/blob/main/Screenshot_20221127-212456.png)
+
+## ⚙️ Installation ⚙️
+
+```python
+
+➤➤ Command ➤➤
+
+➤ termux-setup-storage
+
+➤ apt update
+
+➤ apt upgrade
+
+➤ pkg install python
+
+➤ pkg install git
+
+➤ pip install requests
+
+➤ pip install mechanize
+
+➤ pip install rich
+
+➤ pip install bs4
+
+➤ rm -rf A40➤ git clone  https://github.com/Sami-Ali40/A40.git
+```
+
+#### RUN SCRIPT
+
+```python
+
+➤ cd  A40
+
+➤ mkdir data
+
+➤ python A40py
+
+```
+
